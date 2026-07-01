b063318364277c949a6df5074dd907d8152129e7
Markus Tavenrath <matavenrath@nvidia.com>
2026-07-01 11:05:35 +0200

    Synchronize in trtx::writeTensor to avoid race conditions.

--- src/backends/trtx.rs
+++ src/backends/trtx.rs
@@ -574,6 +574,7 @@ impl<'context> MLBackendContext<'context> for TrtxContext<'context> {
         stream
             .memcpy_htod(array, &mut cuda_tensor.memory)
             .to_write_tensor_result(|| tensor.clone())?;
+        stream.synchronize().to_write_tensor_result(|| tensor.clone())?;
         Ok(())
     }
 
